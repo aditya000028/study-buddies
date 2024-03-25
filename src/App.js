4ce0fa464f468c844b00b18b301eceaccaafba0c
Aditya Gupta <aditya.gupta_chd@yahoo.ca>
2024-03-24 20:08:46 -0700

    Added pages and navigation to pages

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProfilePage from "./pages/Profile";
 import MainNavigation from "./components/layout/navigation/MainNavigation";
+import GroupsPage from "./pages/Groups";
+import MessagesPage from "./pages/Messages";
+import FriendsPage from "./pages/Friends";
+import NotificationsPage from "./pages/Notifications";
+import CalendarPage from "./pages/Calendar";
+import FilesPage from "./pages/Files";
 
 Amplify.configure(awsExports);
 
@@ -21,6 +27,12 @@ export default function App() {
           <div className="page">
             <Routes>
               <Route exact path="/" element={<HomePage />} />
+              <Route path="/groups" element={<GroupsPage />} />
+              <Route path="/messages" element={<MessagesPage />} />
+              <Route path="/friends" element={<FriendsPage />} />
+              <Route path="/notifications" element={<NotificationsPage />} />
+              <Route path="/calendar" element={<CalendarPage />} />
+              <Route path="/files" element={<FilesPage />} />
               <Route path="/profile" element={<ProfilePage />} />
             </Routes>
           </div>
--- src/data/navigation/NavigationData.js
+++ src/data/navigation/NavigationData.js
@@ -8,8 +8,38 @@ export const NAVIGATION_DATA = [
     icon: <FaHome />
   },
   {
+    title: "Groups",
+    path: "/groups",
+    icon: <FaHome />
+  },
+  {
+    title: "Messages",
+    path: "/messages",
+    icon: <FaHome />
+  },
+  {
+    title: "Friends",
+    path: "/friends",
+    icon: <FaHome />
+  },
+  {
+    title: "Notifications",
+    path: "/notifications",
+    icon: <FaHome />
+  },
+  {
+    title: "Calendar",
+    path: "/calendar",
+    icon: <FaHome />
+  },
+  {
+    title: "Files",
+    path: "/files",
+    icon: <FaHome />
+  },
+  {
     title: "Profile",
     path: "/profile",
-    icon: <FaUserCircle />
+    icon: <FaHome />
   },
 ];
